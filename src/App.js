554d2147bfd5561583e742836fc99877f6c771ef
Brian Hestir <brhestir@gmail.com>
2021-02-17 21:14:01 -0500

    Removing boilerplate from app.js

--- src/App.js
+++ src/App.js
@@ -5,21 +5,7 @@ import Container from "./components/Container/Container"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={E8} className="App-logo" alt="logo" />
-        <p>
-          Behold, it moves
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          woah man
-        </a>
-				<Container />
-      </header>
+      <Container />
     </div>
   );
 }
